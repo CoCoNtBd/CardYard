839c2a063d8f9a79f2dbfacbc93260ba18851c03
Naoufel951 <naoufel.boutih@gmail.com>
2025-04-16 19:03:25 +0200

    Update cards.c

--- cards.c
+++ cards.c
@@ -1,1 +1,29 @@
 
+int demander_valeur() {
+    int valeur;
+
+    printf("entrer une valeur : ");
+    scanf("%d", &valeur);
+    if (valeur != 1) {
+        printf( "Erreur\n");
+    }
+
+    return valeur;
+}
+
+void affiche_carte(int carte[]){
+    for(int i=0; i<nombre de carte; i++){
+    printf("[ %d ]", carte[i]);
+    }
+}
+
+void melangerCartes(int cartes[], int nombre de carte) {
+    for (int i = nombre de carte - 1; i > 0; i--) {
+        
+        int j = rand() % (i + 1);
+
+        Carte temp = cartes[i];
+        cartes[i] = cartes[j];
+        cartes[j] = temp;
+    }
+}
