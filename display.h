01eec0a2ae8ab6fd800d7a08087f38e41cecbae1
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-03 00:06:26 +0200

    Update display.h

--- display.h
+++ display.h
@@ -1,1 +1,13 @@
 
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "game.h"
+#include "player.h"
+#include "cards.h"
+
+void afficher_carte(const Carte* carte);
+void afficher_plateau(const Jeu* jeu);
+void afficher_scores(const Jeu* jeu, const int* scores);
+
+#endif
