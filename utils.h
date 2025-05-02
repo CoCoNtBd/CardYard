14326f444f94a9de59070909cb0a1b349d02d088
chleuh <133526876+chleuh@users.noreply.github.com>
2025-05-02 10:27:13 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,26 +1,11 @@
-#ifndef UTILS_H
-#define UTILS_H
+#pragma once
 
-#include <stdbool.h>
-
-void clearScreen();
-void pauseScreen();
-int getRandomInt(int min, int max);
-
-typedef enum {
-    MODE_STANDARD,
-    MODE_VALUE_FILE,
-    MODE_VALUE_USER,
-    MODE_CARD_USER,
-    MODE_CARD_RAND
-} GameMode;
-
-int* loadValuesFromFile(const char* filename, int* totalCards);
-int* getUserDefinedValues(int* totalCards);
-
-int getCustomCardCount();
-int getRandomCardCount(int min, int max);
-
-void applyGameModeSettings(GameMode mode);
-
-#endif
+void clearScreen();                      
+void pauseScreen();                     
+int getRandomInt(int min, int max);      
+void errorHandler(int errorCode);        
+int getValidatedInput(const char* prompt, int min, int max);  
+int* chargerValeursFichier(const char* nomFichier, int* totalCartes);  
+int* demanderValeursUtilisateur(int* totalCartes);
+int demanderNbCartes();                  
+int genererNbCartesAleatoire(int min, int max);  
