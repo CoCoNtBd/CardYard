aa43bfb8ee6bd0ab33aa72ca2548e2f538e7bcc5
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-02 10:06:18 +0200

    Update save.h

--- save.h
+++ save.h
@@ -1,1 +1,5 @@
-
+#include "game.h"   
+#include "player.h" 
+#include "cards.h"  
+int sauvegarder_jeu(const Jeu* jeu, const char* nom_fichier);
+int charger_jeu(Jeu* jeu, const char* nom_fichier);
