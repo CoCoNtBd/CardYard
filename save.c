6f626d5c65f684b865ea3eb20f27db094b8bc4ee
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-02 10:23:29 +0200

    Update save.c

--- save.c
+++ save.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int sauvegarder_jeu(const Jeu* jeu, const char* nom_fichier) {
+    FILE* f = fopen(nom_fichier, "w");
+    if (!f) return 0;
+
+    // Sauvegarde des données du jeu
+    fprintf(f, "%d %d\n", jeu->nb_joueurs, jeu->nb_pioche);
+    for (int i = 0; i < jeu->nb_pioche; ++i)
+        fprintf(f, "%d %d\n", jeu->pioche[i].valeur, jeu->pioche[i].visible);
+
+    for (int i = 0; i < jeu->nb_joueurs; ++i) {
+        Joueur* j = &jeu->joueurs[i];
+        fprintf(f, "%s %d %d\n", j->nom, j->nb_cartes, j->nb_defausse);
+        for (int k = 0; k < j->nb_cartes; ++k)
+            fprintf(f, "%d %d\n", j->cartes[k].valeur, j->cartes[k].visible);
+        for (int k = 0; k < j->nb_defausse; ++k)
+            fprintf(f, "%d %d\n", j->defausse[k].valeur, j->defausse[k].visible);
+    }
+
+fclose(f);
+    return 1;
+}
+
+int charger_jeu(Jeu* jeu, const char* nom_fichier) {
+    FILE* f = fopen(nom_fichier, "r");
+    if (!f) return 0;
+
+    fscanf(f, "%d %d", &jeu->nb_joueurs, &jeu->nb_pioche);
+    jeu->pioche = malloc(jeu->nb_pioche * sizeof(Carte));
+    for (int i = 0; i < jeu->nb_pioche; ++i)
+        fscanf(f, "%d %d", &jeu->pioche[i].valeur, &jeu->pioche[i].visible);
+
+    jeu->joueurs = malloc(jeu->nb_joueurs * sizeof(Joueur));
+    for (int i = 0; i < jeu->nb_joueurs; ++i) {
+        Joueur* j = &jeu->joueurs[i];
+        fscanf(f, "%s %d %d", j->nom, &j->nb_cartes, &j->nb_defausse);
+
+        j->cartes = malloc(j->nb_cartes * sizeof(Carte));
+        for (int k = 0; k < j->nb_cartes; ++k)
+            fscanf(f, "%d %d", &j->cartes[k].valeur, &j->cartes[k].visible);
+
+        j->defausse = malloc(j->nb_defausse * sizeof(Carte));
+        for (int k = 0; k < j->nb_defausse; ++k)
+            fscanf(f, "%d %d", &j->defausse[k].valeur, &j->defausse[k].visible);
+    }
+
+    jeu->tour_actuel = 0;
+    jeu->jeu_termine = 0;
+
+    fclose(f);
+    return 1;
+}
