b2e263de56614287209df614af067888918ef5d3
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-03 19:17:23 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,11 +1,15 @@
-#pragma once
+#ifndef UTILS_H
+#define UTILS_H
 
-void clearScreen();                      
-void pauseScreen();                     
-int getRandomInt(int min, int max);      
-void errorHandler(int errorCode);        
-int getValidatedInput(const char* prompt, int min, int max);  
-int* chargerValeursFichier(const char* nomFichier, int* totalCartes);  
-int* demanderValeursUtilisateur(int* totalCartes);
-int demanderNbCartes();                  
-int genererNbCartesAleatoire(int min, int max);  
+// Vide le buffer de saisie standard (stdin)
+void vider_buffer();
+
+// Demande un entier à l'utilisateur entre min et max (inclus)
+// Affiche un message d'invite
+int demander_entier(const char* message, int min, int max);
+
+// Demande une chaîne de caractères à l'utilisateur
+// Le résultat est stocké dans `buffer` (de taille `taille_max`)
+void demander_chaine(const char* message, char* buffer, int taille_max);
+
+#endif
