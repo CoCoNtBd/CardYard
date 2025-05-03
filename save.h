cf3f3ae56c689e726f14cab762b232a8bc43d9aa
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-03 19:16:43 +0200

    Update save.h

--- save.h
+++ save.h
@@ -1,5 +1,14 @@
-#include "game.h"   
-#include "player.h" 
-#include "cards.h"  
+#ifndef SAVE_H
+#define SAVE_H
+
+#include "game.h"
+
+// Sauvegarde l'état du jeu dans un fichier texte
+// Retourne 1 si succès, 0 si échec
 int sauvegarder_jeu(const Jeu* jeu, const char* nom_fichier);
+
+// Charge l'état du jeu depuis un fichier texte
+// Retourne 1 si succès, 0 si échec
 int charger_jeu(Jeu* jeu, const char* nom_fichier);
+
+#endif
