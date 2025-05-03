cc79c02a5da8a01c99c94831ce9aa1ba385f9401
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-03 19:13:20 +0200

    Update display.h

--- display.h
+++ display.h
@@ -1,4 +1,3 @@
-
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
@@ -6,8 +5,18 @@
 #include "player.h"
 #include "cards.h"
 
+// Affiche une carte individuelle
 void afficher_carte(const Carte* carte);
+
+// Affiche tout le plateau de jeu (joueurs + cartes + défausse + pioche)
 void afficher_plateau(const Jeu* jeu);
+
+// Affiche les scores de fin de partie
 void afficher_scores(const Jeu* jeu, const int* scores);
+
+// (Facultatif mais utile pour debug ou affichage individuel)
+void afficher_joueur(const Joueur* joueur);
+void afficher_defausse(const Carte* defausse, int taille);
+void afficher_pioche(const Carte* pioche, int taille);
 
 #endif
