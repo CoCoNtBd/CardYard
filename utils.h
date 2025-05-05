8ca03e3d111d5540eae108fece7ee00936adc893
CoCoNtBd <corentin.bernard1706@gmail.com>
2025-05-05 19:01:20 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -3,11 +3,9 @@
 
 // Vide le buffer de saisie standard (stdin)
 void vider_buffer();
-
 // Demande un entier à l'utilisateur entre min et max (inclus)
 // Affiche un message d'invite
 int demander_entier(const char* message, int min, int max);
-
 // Demande une chaîne de caractères à l'utilisateur
 // Le résultat est stocké dans `buffer` (de taille `taille_max`)
 void demander_chaine(const char* message, char* buffer, int taille_max);
