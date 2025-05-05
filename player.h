26a7e0d9e3bfc55e040206823bbe064e16fdb2be
CoCoNtBd <corentin.bernard1706@gmail.com>
2025-05-05 18:59:39 +0200

    Update player.h

--- player.h
+++ player.h
@@ -1,6 +1,5 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-
 #include "cards.h"
 
 typedef struct {
@@ -14,13 +13,10 @@ typedef struct {
 
 // Initialise un joueur avec un nombre de cartes
 void initialiser_joueur(Joueur* joueur, int nb_cartes);
-
 // Vérifie si toutes les cartes du joueur sont visibles
 int toutes_cartes_visibles(const Joueur* joueur);
-
 // Ajoute une carte dans la défausse du joueur
 void ajouter_a_defausse(Joueur* joueur, Carte c);
-
 // Échange une carte d'un joueur avec une nouvelle
 void echanger_carte(Joueur* joueur, Carte nouvelle, int index_carte);
 
