0d68b15528ad1333382b3f895119a4c7eac12b3a
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-06 01:59:50 +0200

    Update display.c

--- display.c
+++ display.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "display.h"
 
- // largeur pour les ecart en colonne
-#define ecart 12
+// ecart entre les colonnes
+#define ecart 12 
 
 void afficher_carte(const Carte* carte) {
     if (carte->visible) {
@@ -14,15 +14,17 @@ void afficher_carte(const Carte* carte) {
 
 void afficher_plateau(const Jeu* jeu) {
     printf("\n============================== PLATEAU DE JEU ==============================\n\n");
-    // Cartes et défausse associées à chaque joueur
-    for (int i = 0; i < jeu->nb_joueurs; ++i) {
+
+    // creation carte et defausse de chaque joueur
+    for (int i = 0; i < jeu->nb_joueurs; i++) {
         Joueur* joueur = &jeu->joueurs[i];
         printf("%-10s : ", joueur->nom);
-        for (int j = 0; j < joueur->nb_cartes; ++j) {
+        for (int j = 0; j < joueur->nb_cartes; j++) {
             afficher_carte(&joueur->cartes[j]);
             printf(" ");
         }
-        // Affichage de la défausse du joueur
+
+        // affichage de la defausse
         printf("   Défausse : ");
         if (joueur->nb_defausse > 0) {
             Carte top = joueur->defausse[joueur->nb_defausse - 1];
@@ -32,14 +34,16 @@ void afficher_plateau(const Jeu* jeu) {
         }
         printf("\n");
     }
-    // Pioche centrale
+
+    // pioche centrale
     printf("\nPioche centrale : ");
     if (jeu->nb_pioche > 0) {
-        printf("[ ? ] (%d carte%s)\n", jeu->nb_pioche, jeu->nb_pioche > 1 ? "s" : "");
+        printf("[ ? ] (%d carte)\n", jeu->nb_pioche);
     } else {
         printf("[ Vide ]\n");
     }
-    // Tour actuel
+
+    // les tours
     printf("Tour actuel : Joueur %d (%s)\n", jeu->tour_actuel + 1, jeu->joueurs[jeu->tour_actuel].nom);
     printf("============================================================================\n");
 }
@@ -48,44 +52,11 @@ void afficher_plateau(const Jeu* jeu) {
 
 void afficher_scores(const Jeu* jeu, const int* scores) {
     printf("\n--- Scores des joueurs ---\n");
-    for (int i = 0; i < jeu->nb_joueurs; ++i) {
-        printf("%s : %d point%s\n", jeu->joueurs[i].nom, scores[i], scores[i] > 1 ? "s" : "");
-    }
-}
-
-// affichage individuel d’un joueur
-void afficher_joueur(const Joueur* joueur) {
-    printf("Joueur : %s\n", joueur->nom);
-    printf("Cartes : ");
-    for (int i = 0; i < joueur->nb_cartes; ++i) {
-        afficher_carte(&joueur->cartes[i]);
-        printf(" ");
-    }
-    printf("\n");
-    printf("Défausse : ");
-    if (joueur->nb_defausse > 0) {
-        Carte top = joueur->defausse[joueur->nb_defausse - 1];
-        printf("[ %2d ]\n", top.valeur);
-    } else {
-        printf("[ Vide ]\n");
-    }
-}
-
-void afficher_defausse(const Carte* defausse, int taille) {
-    printf("Défausse : ");
-    if (taille > 0) {
-        Carte top = defausse[taille - 1];
-        printf("[ %2d ] (%d carte%s)\n", top.valeur, taille, taille > 1 ? "s" : "");
-    } else {
-        printf("[ Vide ]\n");
-    }
-}
-
-void afficher_pioche(const Carte* pioche, int taille) {
-    printf("Pioche centrale : ");
-    if (taille > 0) {
-        printf("[ ? ] (%d carte%s)\n", taille, taille > 1 ? "s" : "");
-    } else {
-        printf("[ Vide ]\n");
+    for (int i = 0; i < jeu->nb_joueurs; i++) {
+        printf("%s : %d point", jeu->joueurs[i].nom, scores[i]);
+        if (scores[i] > 1) {
+            printf("s");
+        }
+        printf("\n");
     }
 }
