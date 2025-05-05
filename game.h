5d568d6d0a17e009e7a9874f49447e77f8cf603b
CoCoNtBd <corentin.bernard1706@gmail.com>
2025-05-05 18:57:39 +0200

    Update game.h

--- game.h
+++ game.h
@@ -1,6 +1,5 @@
 #ifndef GAME_H
 #define GAME_H
-
 #include "cards.h"
 #include "player.h"
 
@@ -18,19 +17,14 @@ typedef struct {
     int tours_restants;
 } Jeu;
 
-
 // Initialise une partie complète
 void initialiser_jeu(Jeu* jeu, int nb_joueurs, int nb_cartes_par_joueur);
-
 // Distribue les cartes de la pioche à chaque joueur
 void distribuer_cartes(Jeu* jeu);
-
 // Joue un tour complet pour le joueur courant
 void jouer_tour(Jeu* jeu);
-
 // Vérifie si la partie est terminée (un joueur a toutes ses cartes visibles)
 int verifier_fin_partie(Jeu* jeu);
-
 // Calcule les scores finaux et les stocke dans un tableau externe
 void calculer_scores(const Jeu* jeu, int* scores);
 
