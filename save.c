b3d0d1af496550c1f0334f7b54eea7bf7e75324e
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-06 02:03:07 +0200

    Update save.c

--- save.c
+++ save.c
@@ -9,9 +9,12 @@
 int sauvegarder_jeu(const Jeu* jeu, const char* nom_fichier) {
     FILE* f = fopen(nom_fichier, "w");
     if (!f) return 0;
+
     fprintf(f, "%d %d\n", jeu->nb_joueurs, jeu->nb_pioche);
+
     for (int i = 0; i < jeu->nb_pioche; ++i)
         fprintf(f, "%d %d\n", jeu->pioche[i].valeur, jeu->pioche[i].visible);
+
     for (int i = 0; i < jeu->nb_joueurs; ++i) {
         Joueur* j = &jeu->joueurs[i];
         fprintf(f, "%s %d %d\n", j->nom, j->nb_cartes, j->nb_defausse);
@@ -20,6 +23,7 @@ int sauvegarder_jeu(const Jeu* jeu, const char* nom_fichier) {
         for (int k = 0; k < j->nb_defausse; ++k)
             fprintf(f, "%d %d\n", j->defausse[k].valeur, j->defausse[k].visible);
     }
+
     fprintf(f, "%d\n", jeu->tour_actuel);
     fclose(f);
     return 1;
@@ -33,17 +37,21 @@ int charger_jeu(Jeu* jeu, const char* nom_fichier) {
     jeu->pioche = malloc(jeu->nb_pioche * sizeof(Carte));
     for (int i = 0; i < jeu->nb_pioche; ++i)
         fscanf(f, "%d %d", &jeu->pioche[i].valeur, &jeu->pioche[i].visible);
+
     jeu->joueurs = malloc(jeu->nb_joueurs * sizeof(Joueur));
     for (int i = 0; i < jeu->nb_joueurs; ++i) {
         Joueur* j = &jeu->joueurs[i];
         fscanf(f, "%s %d %d", j->nom, &j->nb_cartes, &j->nb_defausse);
+
         j->cartes = malloc(j->nb_cartes * sizeof(Carte));
         for (int k = 0; k < j->nb_cartes; ++k)
             fscanf(f, "%d %d", &j->cartes[k].valeur, &j->cartes[k].visible);
+
         j->defausse = malloc(j->nb_defausse * sizeof(Carte));
         for (int k = 0; k < j->nb_defausse; ++k)
             fscanf(f, "%d %d", &j->defausse[k].valeur, &j->defausse[k].visible);
     }
+
     fscanf(f, "%d", &jeu->tour_actuel);
     jeu->jeu_termine = 0;
     jeu->tours_restants = -1;
