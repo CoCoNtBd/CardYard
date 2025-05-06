35a57f19a8e5b8b7790bf186c93756057147af54
chleuh <133526876+chleuh@users.noreply.github.com>
2025-05-06 08:16:12 +0200

    Update cards.h

--- cards.h
+++ cards.h
@@ -5,15 +5,11 @@ typedef struct {
     int valeur;
     int visible; // 0 = carte de dos, 1 = carte visible
 } Carte;
-
 // création et manipulation de cartes
 Carte creer_carte(int valeur, int visible);
-
 // melange aléatoire de la pioche
 void melanger_pioche(Carte* pioche, int taille);
-
 // genere la pioche avec les valeur par defaut
 Carte* generer_pioche_defaut(int* taille_pioche);
-
 
 #endif
