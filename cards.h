1634172c9d9fbeb5908c2e876608942e91683a27
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-09 22:39:33 +0200

    Update cards.h

--- cards.h
+++ cards.h
@@ -1,22 +1,22 @@
 #ifndef CARDS_H
 #define CARDS_H
 
+// Structure représentant une carte du jeu
 typedef struct {
-    int valeur;
-    int visible; // 0 = carte de dos, 1 = carte visible
+    int valeur;    // Valeur numérique de la carte (ex : -2, 0, 5, 12)
+    int visible;   // 0 = carte face cachée (dos), 1 = carte visible (face)
 } Carte;
 
-// création et manipulation de cartes
+// Crée une carte avec une valeur donnée et un état (visible ou non)
 Carte creer_carte(int valeur, int visible);
 
-// melange aléatoire de la pioche
+// Mélange les cartes de la pioche de façon aléatoire
 void melanger_pioche(Carte* pioche, int taille);
 
-// genere la pioche avec les valeur par defaut
+// Génère la pioche de cartes avec les valeurs et quantités par défaut
 Carte* generer_pioche_defaut(int* taille_pioche);
 
+// Génère une pioche personnalisée selon les choix de l'utilisateur
 Carte* generer_pioche_personnalisee(int* taille_pioche);
-
-
 
 #endif
