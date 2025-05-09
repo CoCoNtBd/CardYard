8c29387813f7f6ff2313f29e5b636a2ce3d484c6
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-09 14:25:13 +0200

    Update cards.c

--- cards.c
+++ cards.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include "cards.h"
-// on creer une carte avec valeur et visibilité
+#include "utils.h"
+
 Carte creer_carte(int valeur, int visible) {
     Carte c;
     c.valeur = valeur;
     c.visible = visible;
     return c;
 }
-// on melange les cartes de la pioche
+
 void melanger_pioche(Carte* pioche, int taille) {
-    srand(time(NULL)); 
+    srand(time(NULL));
     for (int i = 0; i < taille - 1; i++) {
         int j = i + rand() % (taille - i);
         Carte temp = pioche[i];
@@ -19,21 +20,59 @@ void melanger_pioche(Carte* pioche, int taille) {
         pioche[j] = temp;
     }
 }
-// oon genere la pioche par rapport au valeur et au nombre de carte par valeur
+
 Carte* generer_pioche_defaut(int* taille_pioche) {
     int valeurs[]   = {-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
     int quantites[] = { 5, 10,15,10,10,10,10,10,10,10,10,10,10,10,10};
+
     int total = 0;
-    for (int i = 0; i < 15; i++){
-         total += quantites[i];
-}
+    for (int i = 0; i < 15; i++) {
+        total += quantites[i];
+    }
+
     Carte* pioche = malloc(total * sizeof(Carte));
     int index = 0;
 
     for (int i = 0; i < 15; i++) {
         for (int j = 0; j < quantites[i]; j++) {
-            pioche[index] = creer_carte(valeurs[i], 0); // face cachée
-            index++;
+            pioche[index++] = creer_carte(valeurs[i], 0);
+        }
+    }
+
+    *taille_pioche = total;
+    return pioche;
+}
+
+Carte* generer_pioche_personnalisee(int* taille_pioche) {
+    int choix;
+    do {
+        printf("Choisissez une méthode pour définir les cartes :\n");
+        printf("1. Même quantité pour chaque valeur\n");
+        printf("2. Quantité personnalisée pour chaque valeur\n");
+        choix = demander_entier_secure("Méthode", 1, 2);
+    } while (choix < 1 || choix > 2);
+
+    int valeurs[31];
+    int quantites[31];
+    int total = 0;
+
+    for (int i = 0; i <= 30; i++) {
+        valeurs[i] = i - 10;
+        if (choix == 1) {
+            quantites[i] = 5;
+        } else {
+            // Utilisation d'un message fixe sans snprintf()
+            printf("Combien de cartes pour la valeur %d : ", valeurs[i]);
+            quantites[i] = demander_entier_secure("", 0, 50);
+        }
+        total += quantites[i];
+    }
+
+    Carte* pioche = malloc(total * sizeof(Carte));
+    int index = 0;
+    for (int i = 0; i <= 30; i++) {
+        for (int j = 0; j < quantites[i]; j++) {
+            pioche[index++] = creer_carte(valeurs[i], 0);
         }
     }
 
