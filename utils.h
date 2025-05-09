3829927377d5ed6417af5ec5d94b489326d186fe
Naoufel951 <naoufel.boutih@gmail.com>
2025-05-09 22:48:18 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -2,17 +2,22 @@
 #define UTILS_H
 
 // Vide le buffer de saisie standard (stdin)
+// Utile après un scanf pour éviter que des caractères résiduels restent dans le flux d'entrée
 void vider_buffer();
 
-// Demande un entier à l'utilisateur entre min et max (inclus)
-// Affiche un message d'invite
+// Demande à l'utilisateur de saisir un entier dans un intervalle [min, max]
+// Affiche le message donné en paramètre
+// Repose sur scanf et vide ensuite le buffer
 int demander_entier(const char* message, int min, int max);
 
+// Variante plus sécurisée de demander_entier
+// Gère les erreurs de saisie plus proprement
+// Recommandée pour une utilisation dans des boucles ou des menus
 int demander_entier_secure(const char* message, int min, int max);
 
-
-// Demande une chaîne de caractères à l'utilisateur
-// Le résultat est stocké dans `buffer` (de taille `taille_max`)
+// Demande à l'utilisateur de saisir une chaîne de caractères (ex : nom)
+// Stocke la chaîne saisie dans `buffer`, avec taille maximale `taille_max`
+// Utilise fgets pour éviter les dépassements de tampon
 void demander_chaine(const char* message, char* buffer, int taille_max);
 
 #endif
